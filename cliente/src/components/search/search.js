62428d87e37ed372d5c7b9c15c1d2f940e5e60f4
RamsPantoja <worddraco1@gmail.com>
2019-08-14 13:31:08 -0500

    Estos archivos agregan y modifican el componente search de la aplicacion Agregando un <form> con <input> y <i> icon dentro del mismo. asi como modificando la hoja de estilos del componente search.js

--- cliente/src/components/search/search.js
+++ cliente/src/components/search/search.js
@@ -1,10 +1,18 @@
 import React, {Fragment} from 'react';
+import './search.css';
 
 class Search extends React.Component {
     render() {
         return(
             <Fragment>
-                <div className='search'>Search</div>
+                <div className='search'>
+                    <div className='search-icon-input'>
+                        <form className='search-icon-input-form'>
+                            <i className="material-icons">search</i>
+                            <input type="search" placeholder='Buscar'/>
+                        </form>  
+                    </div>
+                </div>
             </Fragment>
         );
     }
