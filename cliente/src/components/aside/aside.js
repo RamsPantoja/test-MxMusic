8c08469a697a4d923b5e7de809c9bb0420a93285
RamsPantoja <worddraco1@gmail.com>
2019-08-27 14:48:03 -0500

    Se ha diseñado el componente Aside donde se muestran los datos del Usuario asi como su foto de perfil. Tambien se agregaron unos gradientes a la plantilla en los componentes, Drawer y Aside, respectivamente.

--- cliente/src/components/aside/aside.js
+++ cliente/src/components/aside/aside.js
@@ -1,10 +1,18 @@
 import React, {Fragment} from 'react';
+import './aside.css';
 
 class Aside extends React.Component {
     render() {
         return(
             <Fragment>
-                <div className='aside'>Aside</div>
+                <div className='aside'>
+                    <div className='user-info'>
+                        <img src="https://wallpapercave.com/wp/5vKVIBr.jpg" alt="img-dragon-nest-priest"/>
+                        <span>UserName</span>
+                        <span>PREMIUM</span>
+                        <span className='box-connect-button'><a href="google.com" className='connect-button'>Conectar</a></span>
+                    </div>
+                </div>
             </Fragment>
         );
     }
