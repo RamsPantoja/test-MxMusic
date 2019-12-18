b633b2f731b8475ec5c42f623895db13e9b459c4
RamsPantoja <worddraco1@gmail.com>
2019-12-18 02:13:40 -0600

    se colocaron todos los archivos sin exepcion alguna...

---
+++ cliente/build/precache-manifest.889b04b1ffacc8176055eaaed75a9abf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf640a13fe3b50d3adb6325610171a2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef0f3865b3f9ba43ba8f",
+    "url": "/static/css/main.a361ea76.chunk.css"
+  },
+  {
+    "revision": "79347bce0482bcc6b7ba",
+    "url": "/static/js/2.5f5d01ab.chunk.js"
+  },
+  {
+    "revision": "ef0f3865b3f9ba43ba8f",
+    "url": "/static/js/main.dbaf25c9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
